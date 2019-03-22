2259a8f24a014038db26684c3afa737073e9af47
AndreaMendi <andreamendi@icloud.com>
2019-03-21 21:35:48 -0600

    Now getSingleProduct works with the mongoose

--- api/products/getSingleProduct.js
+++ api/products/getSingleProduct.js
@@ -1,21 +1,23 @@
-const Product = require('../../db.js')
+const Product = require('../models/product');
 
 function getSingleProducts (req, res){
 
-    
+    Product.findById(req.params.id)
+        .then(doc => {
+            if (doc){
+                res.status(200).json(doc);
+            } else {
+                res.staurs(400).json({
+                    message: `Sorry, pero el ${req.params.id} que buscas no existe`
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'El formato de id está mal',
+                error: err
+            })
+        })
 }
 
 module.exports = getSingleProducts;
-
-// ----------'FOR'-----------
-    //Se busca el id del req en la lista de ITEMS 
-    // for(let item of products.items){
-    //     if (item.id == productId){
-    //         res.json(item).status(200);
-    //     }
-    // }
-
-    // ----------'FILTER'-----------
-    // const requestedProduct = products.items.filter(function(item){
-    //     return item.id == productId;
-    // })
