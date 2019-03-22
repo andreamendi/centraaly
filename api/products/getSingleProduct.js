a952e6632035b80b32bc3200b962003ace753158
AndreaMendi <andreamendi@icloud.com>
2019-03-21 21:22:48 -0600

    Merge with sinMongo

--- api/products/getSingleProduct.js
+++ api/products/getSingleProduct.js
@@ -1,27 +1,6 @@
-const products = require('../../db.js')
+const Product = require('../../db.js')
 
 function getSingleProducts (req, res){
-    //Accedemos al id que viene en el req
-    const productId = req.params.id;
-    // ----------'FOR'-----------
-    //Se busca el id del req en la lista de ITEMS 
-    // for(let item of products.items){
-    //     if (item.id == productId){
-    //         res.json(item).status(200);
-    //     }
-    // }
-
-    // ----------'FILTER'-----------
-    // const requestedProduct = products.items.filter(function(item){
-    //     return item.id == productId;
-    // })
-
-    const requestedProduct = products.items.filter(item => item.id == productId)
-    if(requestedProduct.length != 0){
-        res.json(requestedProduct).status(200);
-    } else{
-        res.send(`No se encontró el producto con id: ${productId}`).status(400);
-    }
 
 
 }
